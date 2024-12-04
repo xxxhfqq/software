dd02858b0f973bde77cc5d701b62c9a139b0536e
xxxhf <2731897659@qq.com>
2024-12-04 13:18:11 +0800

    1

--- demo/src/main/java/com/example/demo/NoteController.java
+++ demo/src/main/java/com/example/demo/NoteController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class NoteController {
     @GetMapping("/note")
